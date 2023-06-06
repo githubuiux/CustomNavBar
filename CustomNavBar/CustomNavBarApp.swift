3676299c812f531350778f79169baa46953a2234
githubuiux <uiuxista@gmail.com>
2023-06-06 20:53:38 +0300

    Initial Commit

---
+++ CustomNavBar/CustomNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavBarApp.swift
+//  CustomNavBar
+//
+//  Created by Levent on 6.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
